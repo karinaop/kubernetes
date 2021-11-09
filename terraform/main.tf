68d5b612535e2a96663b42c0226f73393b4cecb0
Karina Pereira <karina.pereira93@hotmail.com>
2021-11-09 20:42:33 -0300

    alteracao main

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,6 +75,7 @@ variable "client" {
 variable "secret" {
 }
 # Atividade realizada em grupo, nome dos alunos:
+# Cristielen Oliveira de Barros - RA: 2103005
 # Deivson Sena Teixeira dos Santos - RA: 2102107
 # Greicy Kelly Silva Oliveira - RA: 2102236
 # Karina de Oliveira Pereira - RA: 1200778
